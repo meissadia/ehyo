c26c428f387310d743c1ad1357bdd9c16bc30071
Meissa Dia <meissadia@gmail.com>
2018-03-24 00:05:20 -0700

    Update README. Update git aad.

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Or install it yourself as:
 
 ## Usage
 
-ehyo <command> [options]
+ehyo <command> [options]  
 ehyo help <command>
 
 ## Development
--- lib/ehyo/commands/git.rb
+++ lib/ehyo/commands/git.rb
@@ -1,15 +1,19 @@
 group = 'Git:'
 
-command :'git aac' do |c|
-  c.syntax      = 'ehyo aac [options]'
-  c.summary     = "#{group} Add & Commit"
-  c.description = 'Add all changes to git. Prompt for commit message.'
+command :'git aad' do |c|
+  c.syntax      = 'ehyo aad [options]'
+  c.summary     = "#{group} Add & Deploy"
+  c.description = 'Add all changes to git. Prompt for commit message. Deploy to Github.'
   c.option '--message STRING', String, 'git commit message'
-  c.option '--push', 'push changes to REMOTE'
+  c.option '--xpush', 'DON\'T push changes to Github'
   c.action do |args, options|
     puts "- #{c.summary} -" if c.summary
     msg = options.message || ask(COMMIT_MSG_PROMPT)
+    if msg.empty?
+      puts "User cancelation.\n\n"
+      next
+    end
     git_add_commit(msg)
-    exec 'git push' if options.push
+    exec 'git push' unless options.xpush
   end
 end
